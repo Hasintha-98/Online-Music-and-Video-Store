82ae09809cd707e238ec44738f309e1a82d8e2f1
Hasintha Vishwajith <62957436+Hasintha-98@users.noreply.github.com>
2021-07-19 22:54:48 +0530

    Add files via upload

---
+++ contentDetails.js
@@ -0,0 +1,28 @@
+function cart(){
+	alert("Succesfully Added to Cart!");
+}
+
+function fav(){
+	alert("Succesfully Added to Favourite!");
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  	for (i = 0; i < a.length; i++) {
+    	txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } 
+    else {
+      a[i].style.display = "none";
+    }
+  }
+}
